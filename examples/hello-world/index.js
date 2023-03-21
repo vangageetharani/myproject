bde6b1324e19b3dbabb58d7b8cd9ae35e46dd895
vangageetharani <128458041+vangageetharani@users.noreply.github.com>
2023-03-21 18:48:23 +0530

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -5,7 +5,7 @@ var express = require('../../');
 var app = module.exports = express()
 
 app.get('/', function(req, res){
-  res.send('Hello world 1234');
+  res.send('Hello world 12345');
 });
 
 app.get('/test', function(req, res){
